555f8247ecdc011f986473bbc4bf0ab7c9c71640
muri <kirill89450@gmail.com>
2024-03-13 14:56:58 +0700

    new version ipgrade agent logic

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
-# Параметры игры
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-BLOCK_SIZE = 20
-GAME_SPEED = 20
-FILENAME = "best_score.txt"
+HEIGHT                = 20      # number of steps vertically from wall to wall of screen
+WIDTH                 = 20       # number of steps horizontally from wall to wall of screen
+PIXEL_H               = 20*HEIGHT  # pixel height + border on both sides
+PIXEL_W               = 20*WIDTH   # pixel width + border on both sides
+SLEEP                 = 0.1     # time to wait between steps
 
-# Цвета
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 102)
-BLACK = (0, 0, 0)
-RED = (213, 50, 80)
-GREEN = (0, 255, 0)
-BLUE = (50, 153, 213)
+GAME_TITLE            = 'Snake'
+BG_COLOR              = 'black'
 
-# Параметры нейронной сети
-MEMORY_SIZE = 100000  # Размер памяти для повторного воспроизведения
-INITIAL_EPSILON = 1  # Начальное значение epsilon
-GAMMA = 0.95  # Фактор дисконтирования
-BATCH_SIZE = 500  # Размер пакета для обучения
-MIN_EPSILON = 0.01  # Минимальное значение epsilon
-EPSILON_DECAY = 0.995  # Скорость уменьшения epsilon
-LEARNING_RATE = 0.00025  # Скорость обучения
-LAYER_SIZES = [128, 128, 128]  # Размеры скрытых слоев нейронной сети
-SENSORS = 12  # Количество сенсоров (входных параметров для нейронной сети)
+
+SNAKE_SHAPE           = 'square'
+SNAKE_COLOR           = 'green'
+SNAKE_START_LOC_H     = 0
+SNAKE_START_LOC_V     = 0
+
+APPLE_SHAPE           = 'circle'
+APPLE_COLOR           = 'red'
+
+params = {
+    'epsilon': 1,
+    'gamma': 0.95,
+    'batch_size': 500,
+    'epsilon_min': 0.01,
+    'epsilon_decay': 0.995,
+    'learning_rate': 0.00025,
+    'layer_sizes': [128, 128, 128]
+}
+action_space = 4  # сколько возможных действий
+state_space = 12  # размер пространства состояний
